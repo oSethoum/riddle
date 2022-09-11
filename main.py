61150603b8867fb5f1a357a6b5070fd5fd65df80
oSethoum <os.sethoum@gmail.com>
2022-09-11 07:07:41 +0100

    fix boxes issue

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import random
 
 def main():
     boxes = list(range(0, 100))
-
     safes = 0
     print("opening boxes randomly")
-    for i in range(1000):
+    for i in range(100):
         random.shuffle(boxes)
         if rand(boxes) == 100:
             safes += 1
     print("safes", safes)
 
     print("opening boxes with loop method")
+    boxes = list(range(0, 100))
     safes = 0
-    for i in range(1000):
+    for i in range(100):
         random.shuffle(boxes)
         if loop(boxes) == 100:
             safes += 1
@@ -23,16 +23,14 @@ def main():
 
 def rand(boxes):
     safe = 0
-    for i in range(100):
+    for i in range(len(boxes)):
         opened = []
         while True:
-            box = random.randint(0, 99)
+            box = random.randint(0, len(boxes) - 1)
             if not (box in opened):
                 opened.append(boxes[box])
-
-            if len(opened) == 50:
+            if len(opened) == (len(boxes) / 2):
                 break
-
         if i in opened:
             safe += 1
     return safe
@@ -40,14 +38,14 @@ def rand(boxes):
 
 def loop(boxes):
     safe = 0
-    for i in range(100):
+    for i in range(len(boxes)):
         opened = []
         box = i
         while True:
             content = boxes[box]
             opened.append(content)
             box = content
-            if len(opened) == 50:
+            if len(opened) == len(boxes) / 2:
                 break
         if i in opened:
             safe += 1
